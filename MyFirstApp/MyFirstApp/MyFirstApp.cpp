4eff9917ddf4db99ada5974d5947cf77736b9ceb
Montri <jg68332@gmail.com>
2022-02-13 15:24:13 +0100

    First App

---
+++ MyFirstApp/MyFirstApp/MyFirstApp.cpp
@@ -0,0 +1,18 @@
+// MyFirstApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float annualSalary;
+
+	// Output.
+	cout << "Please enter your annual Salary: ";
+	// Input.
+	cin >> annualSalary;
+	cout << "Your monthly salary is: " << annualSalary / 12 << endl;
+
+	char character = 'a';
+	system("pause>0");
+}
